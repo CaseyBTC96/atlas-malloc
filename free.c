c2659d8567342fdae0d62dfd1fe9cb2fab94ec8c
casey paul <casey.paul@atlasschool.com>
2025-01-29 17:10:30 +0000

    fixed free and added betty

--- free.c
+++ free.c
@@ -1,10 +1,20 @@
 #include "malloc.h"
 
+/**
+* _free - Frees the allocated memory pointed to by ptr.
+*
+* This function adds the block back to the free list.
+*
+* @ptr: Pointer to the memory block to be freed.
+*/
 void _free(void *ptr)
 {
-    if (ptr == NULL)
-        return;
-    Block *block = (Block *)((char *)ptr - sizeof(Block));
-    block->next = free_list;
-    free_list = block;
+	Block *block;
+
+	if (ptr == NULL)
+		return;
+
+	block = (Block *)((char *)ptr - sizeof(Block));
+	block->next = free_list;
+	free_list = block;
 }
