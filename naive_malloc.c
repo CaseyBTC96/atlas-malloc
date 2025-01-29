23f55f56350f0059e3bf83734a6d7dca4ace1778
casey paul <casey.paul@atlasschool.com>
2025-01-29 17:15:32 +0000

    cleaned up and added betty

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,59 +1,38 @@
 #include "malloc.h"
 
-static Block *free_list = NULL; /* The free list for  managing free blocks */
-
-
+/**
+* naive_malloc - Allocates memory in the heap
+* @size: size of memory to allocate
+* Return: returns a pointer to the allocated memory
+*/
 void *naive_malloc(size_t size)
 {
-    static void *heap_end = NULL;
-    void *prev_heap_end;
-    Block *block, *prev = NULL;
-    size_t aligned_size;
+	static void *heap_end;
+	void *prev_heap_end;
+	void *ptr;
+	size_t aligned_size;
 
-    if (size == 0)
-        return NULL;
+	/* heap_end = NULL; */
+	if (size == 0)
+		return (NULL);
 
-    aligned_size = ALIGN_SIZE(size);
+	/* Align size to the next page boundary, with the size of the block hdr */
+	aligned_size = ALIGN_SIZE(size + sizeof(size_t));
 
-    /* Check if the free list has a good sized usable block */
-    for (block = free_list; block != NULL; block = block->next)
-    {
-        if (block->size >= aligned_size)
-        {
-            /* Good sized usable block found */
-            if (block->size > aligned_size + sizeof(Block))
-            {
-                /* Split the block if too big */
-                Block *remaining_block = (Block *)((char *)block + aligned_size + sizeof(Block));
-                remaining_block->size = block->size - aligned_size - sizeof(Block);
-                remaining_block->next = block->next;
-                block->size = aligned_size;
-                block->next = remaining_block;
-            }
-            else
-            {
-                /* Take out the block from the free list */
-                if (prev == NULL)
-                    free_list = block->next;
-                else
-                    prev->next = block->next;
-            }
-            return (char *)block + sizeof(Block);
-        }
-        prev = block;
-    }
+	if (heap_end == NULL)
+		heap_end = sbrk(0);  /* Make the heap_end to what break is right now */
 
-    /* No good block found, make the heap larger */
-    if (heap_end == NULL)
-        heap_end = sbrk(0);  /* Set if first use */
+	prev_heap_end = heap_end;
+	if (sbrk(aligned_size) == (void *)-1)
+		return (NULL);  /* sbrk didnt work */
+	heap_end = (char *)heap_end + aligned_size;  /* Updating the heap_end */
 
-    prev_heap_end = heap_end;
-    if (sbrk(aligned_size + sizeof(Block)) == (void *)-1)
-        return NULL;  /* sbrk failed */
-    heap_end = (char *)heap_end + aligned_size + sizeof(Block);
+	/* Store what size is at the beginning of the block */
+	*(size_t *)prev_heap_end = aligned_size;
 
-    /* Make a new block and use the good part of it */
-    block = (Block *)prev_heap_end;
-    block->size = aligned_size;
-    return (char *)block + sizeof(Block);
+	ptr = (char *)prev_heap_end + sizeof(size_t);
+	/* Store what size is at the beginning of the block */
+	/* *(size_t *)prev_heap_end = aligned_size; */
+	/*  moved  */
+	return (ptr);
 }
